69ad9e1758659d4063385f68850a99dc486da0bf
DBNagy <70963888+DBNagy@users.noreply.github.com>
2020-10-30 14:37:09 -0400

    Added Second Navigation Bar with User Mode Toggle Button
    Moved all of the user pages into a second navigatgion bar which can be toggled on and off in the public pages.

--- index.php
+++ index.php
@@ -11,11 +11,13 @@
   <link rel="stylesheet" href="./css/bootstrap.min.css"/>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/secondnav_toggle.js"></script>
 
 </head>
 
 <body>
-  
+ 
+<div id="main_nav"> 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Team Purple B03</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,16 +28,33 @@
       <a class="nav-link hoverable active" href="./index.php">Home <span class="sr-only">(current)</span></a>
       <a class="nav-link hoverable" href="./pages/faq.php">FAQ</a>
       <a class="nav-link hoverable" href="./pages/contact_us.php">Contact Us</a>
+    </div>
+    <div class="navbar navbar-nav ml-auto p-2" id="navbar">
+      <a class="btn btn-md btn-outline-warning" id="mode_button" onclick="second_navbar()">User Mode</a>
+      <a class="btn btn-md hoverable" data-toggle="modal" data-target="#signupModal">Sign Up</a>
+      <a class="btn btn-md hoverable" data-toggle="modal" data-target="#loginModal">Log In</a>
+    </div>
+  </div>
+</nav>
+</div> 
+
+<div id="user_nav"> 
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup2" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup2">
+    <div class="navbar navbar-nav p-2">
       <a class="nav-link hoverable" href="./pages/add_pet.php">Add a Pet</a>
       <a class="nav-link hoverable" href="./pages/update_pet.php">Update Pet</a>
       <a class="nav-link hoverable" href="./pages/pet_history.php">Pet History</a>
+      <a class="nav-link hoverable" href="./pages/add_vet_service.php">Add Veterinary Service</a>
+      <a class="nav-link hoverable" href="./pages/add_grooming_service.php">Add Grooming Service</a>
+      <a class="nav-link hoverable" href="./pages/add_boarding_service.php">Add Boarding Service</a>
     </div>
-    <div class="navbar navbar-nav ml-auto p-2" id="navbar">
-          <a class="btn btn-md hoverable" data-toggle="modal" data-target="#signupModal">Sign Up</a>
-          <a class="btn btn-md hoverable" data-toggle="modal" data-target="#loginModal">Log In</a>
-      </div>
   </div>
 </nav>
+</div>
   
   <div class="jumbotron jumbotron-sm">
     <div class="container">
