b2ce528844a2df9dd88760f778993c8ff24ab15e
DBNagy <70963888+DBNagy@users.noreply.github.com>
2020-12-02 23:00:49 -0500

    Vaccine checkboxes added to Vet Service, Vet History page created along with DB table and Nav link.

--- pages/add_vet_service.php
+++ pages/add_vet_service.php
@@ -15,14 +15,13 @@
 			} else {
 				$storeValueId;
 				while ($row = sqlsrv_fetch_object($stmt)) {
-					echo "<option id = " . $row->id . " value = " . $row->name . ">" . $row->name . "</option>";
+					echo "<option id = " . $row->id . " value = ". $row->name .">" . $row->name . "</option>";
 				}
 			}
 		} catch (Throwable $e) {
 			echo "Throwable Error: " . $e;
 		}
         sqlsrv_close($conn);
-
     }
 
     //Vet Selector
@@ -63,7 +62,29 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
 </head>
 <script type="text/javascript">
-    $(document).ready(function() {
+	$(document).ready(function() {
+		var idNum = document.getElementById("select_pet_control");
+        var species
+		$("#select_pet_control").change(function(){
+			$.post({
+				url: "../php/retrieve_pet.php",
+				data: {pet_ID: idNum.options[idNum.selectedIndex].id},
+				success: function(feedback){
+					var json = JSON.parse(feedback);
+                    species = json["Species"]; 
+                    if (json["Species"]=="Dog"){
+                        $('#k9_vaccine_checkboxes').show();
+                        $('#feline_vaccine_checkboxes').hide();
+					} else if (json["Species"]=="Cat"){
+                        $('#k9_vaccine_checkboxes').hide();
+                        $('#feline_vaccine_checkboxes').show();
+					}else{
+                        $('#k9_vaccine_checkboxes').hide();
+                        $('#feline_vaccine_checkboxes').hide();
+					}				
+				}
+			});
+		});
         $("#save_service").click(function() {
 
             
@@ -71,13 +92,61 @@
             var location = document.getElementById("select_location_control");
             var text = document.getElementById("detail_entry").value;
 
-			$.post({
+            var k9_vaccines_list = document.getElementsByName("k9_vaccines");
+            for (i = 0; i < k9_vaccines_list.length; i++) {
+                if (k9_vaccines_list[i].checked){
+                    k9_vaccines_list[i].value = 1;
+                }else{
+                    k9_vaccines_list[i].value = 0;
+                }
+            }
+            var feline_vaccines_list = document.getElementsByName("feline_vaccines");
+            for (i = 0; i < feline_vaccines_list.length; i++) {
+                if (feline_vaccines_list[i].checked){
+                    feline_vaccines_list[i].value = 1;
+                }else{
+                    feline_vaccines_list[i].value = 0;
+                }
+            }
+            
+            $.post({
 				url: "../php/add_vet_serviceDB.php", 
                 data: { petId: petChosen.options[petChosen.selectedIndex].id, 
                         serviceDate: document.getElementById("service_date_id").value,
                         locationId: location.options[location.selectedIndex].id,
                         details: text
+                    }, 
+                    success: function() {
+					    $('#addition_successful').modal();
+				    },
+				    error: function(err) {
+					    alert("Err " + err);
+				    }
+			});
 
+			$.post({
+				url: "../php/add_vetHistory_serviceDB.php", 
+                data: { petName: document.getElementById("select_pet_control").value, 
+                        serviceDate: document.getElementById("service_date_id").value,
+                        locationName: document.getElementById("select_location_control").value,
+                        details: text,
+                        k9_rabies: document.getElementById("k9_rabies_Id").value,
+                        k9_distemper: document.getElementById("k9_distemper_Id").value,
+                        k9_parvo: document.getElementById("k9_parvo_Id").value,
+                        k9_adeno1: document.getElementById("k9_adeno1_Id").value,
+                        k9_adeno2: document.getElementById("k9_adeno2_Id").value,
+                        k9_parainfluenza: document.getElementById("k9_parainfluenza_Id").value,
+                        k9_bordetella: document.getElementById("k9_bordetella_Id").value,
+                        k9_lyme: document.getElementById("k9_lyme_Id").value,
+                        k9_leptospirosis: document.getElementById("k9_leptospirosis_Id").value,
+                        k9_influenza: document.getElementById("k9_influenza_Id").value,
+                        feline_rabies: document.getElementById("feline_rabies_Id").value,
+                        feline_distemper: document.getElementById("feline_distemper_Id").value,
+                        feline_herpes: document.getElementById("feline_herpes_Id").value,
+                        feline_calici: document.getElementById("feline_calici_Id").value,
+                        feline_leukemia: document.getElementById("feline_leukemia_Id").value,
+                        feline_bordetella: document.getElementById("feline_bordetella_Id").value,
+                        species: species
                     }, 
                     success: function() {
 					    $('#addition_successful').modal();
@@ -124,6 +193,88 @@
             <input class="form-control col-5 col-sm-7 col-md-6 col-lg-5 col-xl-4" type="date" id="service_date_id">
         </div>
     </div>    
+    <!-- Right Column-->
+	<div class="col-md-6">
+        <div class="row" id="k9_vaccine_checkboxes" style="display: none">
+        <legend>Canine Vaccines</legend>
+            <!-- K9 Vaccines Left Column-->
+            <div class="col">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_rabies_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_rabies_Id">Rabies</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_distemper_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_distemper_Id">Distemper</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_parvo_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_parvo_Id">Parvovirus</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_adeno1_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_adeno1_Id">Adenovirus,  Type 1</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_adeno2_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_adeno2_Id">Adenovirus,  Type 2</label>
+                </div>
+            </div>
+            <!-- K9 Vaccines Right Column-->
+            <div class="col">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_parainfluenza_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_parainfluenza_Id">Parainfluenza</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_bordetella_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_bordetella_Id">Bordetella</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_lyme_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_lyme_Id">Lyme Disease</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_leptospirosis_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_leptospirosis_Id">Leptospirosis</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="k9_influenza_Id" name="k9_vaccines" value="">
+                    <label class="custom-control-label" for="k9_influenza_Id">Canine Influenza</label>
+                </div>
+            </div>
+        </div>
+        <!-- Feline Vaccines-->
+        <div class="row" id="feline_vaccine_checkboxes" style="display: none">
+            <legend>Feline Vaccines</legend>
+            <div class="col">
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_rabies_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_rabies_Id">Rabies</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_distemper_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_distemper_Id">Feline Distemper</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_herpes_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_herpes_Id">Feline Herpesvirus</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_calici_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_calici_Id">Calicivirus</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_leukemia_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_leukemia_Id">Feline Leukemia Virus</label>
+                </div>
+                <div class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" id="feline_bordetella_Id" name="feline_vaccines" value="">
+                    <label class="custom-control-label" for="feline_bordetella_Id">Bordetella</label>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
     <!-- Details -->
