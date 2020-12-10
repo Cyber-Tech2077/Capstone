b9bd63550ac527a7b776f7b63ab2cecf32bbd73b
Garrett Moore <gmoore35@student.cscc.edu>
2020-12-10 15:58:20 -0500

    updated pet

--- php/post-usages/simpleSignUp.php
+++ php/post-usages/simpleSignUp.php
@@ -4,32 +4,44 @@
 
     $conn = databaseConnect('Pet');
     try {
-
-        $query = 'insert into users (username, password, email) values (?,?,?)';
-        $credentials = array();
-        foreach ($_POST as $postKey => $postValue) {
-            foreach (json_decode($postValue) as $key => $value) {
-                switch (strtoupper($key)) {
-                    case 'USERNAME':
-                        array_push($credentials, $value);
-                        break;
-                    case 'PASSWORD':
-                        array_push($credentials, password_hash($value, PASSWORD_BCRYPT));
-                        break;
-                    case 'EMAIL':
-                        array_push($credentials, $value);
-                        break;
-                }
+        
+        $query = 'select [username] from users where [username] = ?';
+        $queryStmt = sqlsrv_query($conn, $query, array(json_decode($_POST['signup'])->username));
+        if ($queryStmt === false) {
+            foreach(sqlsrv_errors() as $errorKey => $errorValue) {
+                echo json_encode(array('error' => 'Please contact administrator.'));
             }
         }
-        $stmt = sqlsrv_prepare($conn, $query, $credentials);
-        $execute = sqlsrv_execute($stmt);
-        if ($execute) {
-            echo json_encode(array('successful' => 'You have been successfully signed up. Please login with your credentials.'));
-        } else {
-            foreach (sqlsrv_errors() as $errorKey => $errorValue) {
-                echo json_encode(array('error' => 'An error has sadly encountered. Please try again later.'));
+        if (sqlsrv_fetch_array($queryStmt, SQLSRV_FETCH_ASSOC) == null) {
+        
+            $query = 'insert into users (username, password, email) values (?,?,?)';
+            $credentials = array();
+            foreach ($_POST as $postKey => $postValue) {
+                foreach (json_decode($postValue) as $key => $value) {
+                    switch (strtoupper($key)) {
+                        case 'USERNAME':
+                            array_push($credentials, $value);
+                            break;
+                        case 'PASSWORD':
+                            array_push($credentials, password_hash($value, PASSWORD_BCRYPT));
+                            break;
+                        case 'EMAIL':
+                            array_push($credentials, $value);
+                            break;
+                    }
+                }
             }
+            $stmt = sqlsrv_prepare($conn, $query, $credentials);
+            $execute = sqlsrv_execute($stmt);
+            if ($execute) {
+                echo json_encode(array('successful' => 'You have been successfully signed up. Please login with your credentials.'));
+            } else {
+                foreach (sqlsrv_errors() as $errorKey => $errorValue) {
+                    echo json_encode(array('error' => 'An error has sadly encountered. Please try again later.'));
+                }
+            }
+        } else {
+            echo json_encode(json_decode('{"error": "Username already exists. Please try using a different username."}'));
         }
     } catch (Exception $e) {
         return 'Exception: ' . $e;
